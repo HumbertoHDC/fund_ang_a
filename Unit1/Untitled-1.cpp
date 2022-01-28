a8891d8eac666c8e209455ae5dae0b956842f006
HumbertoProgrammer <2109080@upy.edu.mx>
2022-01-27 22:10:09 -0600

    Files

---
+++ Unit1/Untitled-1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int num1; //Declarando mi variable
+int num2;
+
+int main(){
+
+    //Imprimir mensaje//
+    printf("Enter number1,\n\tnum1: ");
+    scanf("%d", &num1);
+    printf("Enter number2,ªnªtnum2: ");
+    scanf("%d", &num2);
+    return 0;
+}
